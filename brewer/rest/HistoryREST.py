d05265463a37b59b5e005b162319d0d33b32066e
Nikola Spiric <nikola.spiric@rt-rk.com>
2019-03-23 08:50:34 +0100

    Rework history rest API

--- brewer/rest/HistoryREST.py
+++ brewer/rest/HistoryREST.py
@@ -3,50 +3,18 @@ from ssc.http.HTTP import CODE_OK, MIME_TEXT, MIME_JSON, MIME_HTML, CODE_BAD_REQ
 from brewer.LogHandler import LogHandler
 from brewer.HistoryHandler import HistoryHandler
 from brewer.HardwareHandler import HardwareHandler, ComponentType
+from brewer.rest.BaseREST import BaseREST
 
-
-class HistoryREST:
+class HistoryREST(BaseREST):
     '''
     API used to fetch informatino from history hadnler
     '''
 
     def __init__(self, brewer):
-        self._brewer = brewer
+        BaseREST.__init__(self, brewer, 'history/')
 
-    def getRestAPI(self):
-        '''
-        Create REST API
-        '''
+        self.addAPI('getSamples', self._getSamples)
 
-        return (
-                # Fetch relay state
-                RestHandler(
-                    'history/get',
-                    self._getHistory
-                ),
 
-                RestHandler(
-                    'history/getRecords',
-                    self._getRecords
-                ),
-
-        )
-
-    def _getRecords(self, request):
-        return (CODE_OK, MIME_JSON,
-                            {'success' : True, 'res' : self._brewer.getModule(HistoryHandler).getRecords()})
-
-    def _getHistory(self, request):
-        res = {}
-
-        for component in self._brewer.getModule(HardwareHandler).getComponents():
-
-            if component.componentType == ComponentType.SENSOR:
-                value = component.getValue()
-            elif component.componentType == ComponentType.SWITCH:
-                value = 1.0 if component.isOn() else 0.0
-
-            res[component.name] = {'value' : value, 'type' : component.componentType.name}
-
-        return (CODE_OK, MIME_JSON,
-                                {'success' : True, 'res' : res})
+    def _getSamples(self, request):
+        return self._brewer.getModule(HistoryHandler).getSamples()
